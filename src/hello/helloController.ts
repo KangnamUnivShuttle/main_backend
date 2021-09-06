b007793790e9985e98b108ed9fe7d26a7afc5605
Kim HyunWoo <stories2@naver.com>
2021-09-06 16:53:59 +0900

    tsoa proj init

---
+++ src/hello/helloController.ts
@@ -0,0 +1,13 @@
+import {
+    Controller,
+    Get,
+    Route,
+  } from "tsoa";
+  
+  @Route("hello")
+  export class HelloController extends Controller {
+    @Get()
+    public async hello(): Promise<string> {
+      return 'hello world!';
+    }
+}
