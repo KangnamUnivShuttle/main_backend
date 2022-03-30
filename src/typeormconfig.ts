36e8c99a5c0da6f334214d0127ebecddae47f443
Kim HyunWoo <stories2@naver.com>
2022-03-30 18:37:50 +0900

    Update typeormconfig.ts

--- src/typeormconfig.ts
+++ src/typeormconfig.ts
@@ -1,5 +1,19 @@
 import { ConnectionOptions } from "typeorm";
 import logger from "./logger";
+// https://github.com/nrwl/nx/issues/803#issuecomment-450642765
+
+function entityResolver() {
+  const contexts = (require as any).context('./orm/entities', true, /\.ts$/);
+  console.log('contexts', contexts, contexts.keys())
+  return contexts
+  .keys()
+  .map((modulePath: any) => {
+    console.log('modulePath: ', modulePath)
+    return contexts(modulePath)
+  })
+  .reduce((result: any, entityModule: any) => result.concat(Object.keys(entityModule).map(key => entityModule[key]), []))
+}
+
 const ormconfig: ConnectionOptions = {
     type: "mariadb",
     host: process.env.DB_HOST || 'localhost',
@@ -9,7 +23,7 @@ const ormconfig: ConnectionOptions = {
     database: process.env.DB_DATABASENAME || 'chatbot_system',
     synchronize: false,
     logging: false,
-    entities: process.env.NODE_ENV === 'production' ? [__dirname + '/dist/entities/*.js'] : [__dirname + "/orm/entities/*.{js,ts}", ],//__dirname + ,
+    entities: process.env.NODE_ENV === 'production' ? entityResolver() : [__dirname + "/orm/entities/*.{js,ts}", ],//__dirname + ,
     migrations: [
       __dirname + "/migrations/entities/*.{js,ts}"
     ],
