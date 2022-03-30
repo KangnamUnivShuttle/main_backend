1ac6bdb9bce82057c8b6267bef51961d82bed788
Kim HyunWoo <stories2@naver.com>
2022-03-30 17:36:53 +0900

    asdf

--- src/typeormconfig.ts
+++ src/typeormconfig.ts
@@ -3,19 +3,19 @@ import logger from "./logger";
 const ormconfig: ConnectionOptions = {
     type: "mariadb",
     host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT || 3306),
+    port: Number(process.env.DB_PORT || '3306'),
     username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASENAME || 'chatbot_system',
     synchronize: false,
     logging: false,
-    entities: [__dirname + "/orm/entities/*.{js,ts}", ],//__dirname + ,
+    entities: process.env.NODE_ENV === 'production' ? ['dist/entities/*.js'] : [__dirname + "/orm/entities/*.{js,ts}", ],//__dirname + ,
     migrations: [
       __dirname + "/migrations/entities/*.{js,ts}"
     ],
     cli: {
-        entitiesDir: "src/orm",
-        migrationsDir: "src/migrations"
+        entitiesDir: process.env.NODE_ENV === 'production' ? 'dist/orm' : "src/orm",
+        migrationsDir: process.env.NODE_ENV === 'production' ? 'dist/migrations' : "src/migrations"
     }
   };
 // console.log('asdf', __dirname, __dirname + "src/orm/entities/*.{js,ts}")
