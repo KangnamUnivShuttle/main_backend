a334894a34305160cfae15fcd4ba730b6a8b3182
Kim HyunWoo <stories2@naver.com>
2022-03-30 11:20:32 +0900

    redis

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,9 @@ import session from 'express-session';
 import passportLocal from 'passport-local';
 import logger from './logger';
 import "reflect-metadata";
+import redis from 'redis';
+import connectRedis from 'connect-redis';
+
 const LocalStrategy = passportLocal.Strategy;
 
 export const app = express();
@@ -22,6 +25,20 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+const RedisStore = connectRedis(session);
+const redisUrl = `redis://${process.env.REDIS_HOST || '127.0.0.1'}:${process.env.REDIS_PORT || '6379'}`
+logger.debug(`[App] [Redis-Init] redis url ${redisUrl}`)
+const redisClient = redis.createClient(Number(process.env.REDIS_PORT || '6379'), process.env.REDIS_HOST || '127.0.0.1')
+// redisClient.connect()
+
+redisClient.on('error', (err) => {
+  logger.error(`[App] [RedisClient-error] err: ${err.message}`)
+})
+
+redisClient.on('connect', () => {
+  logger.info(`[App] [RedisClient-connect] redis connected.`)
+})
 
 passport.use(new LocalStrategy(
     {
@@ -41,9 +58,25 @@ passport.use(new LocalStrategy(
         }
     }
 ));
-app.use(session({ secret: process.env.SESSION_KEY || 'testSecret', resave: true, saveUninitialized: false, cookie: {maxAge: Number(process.env.SESSION_TIME) || 1800000 } }));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(session({ 
+  secret: process.env.SESSION_KEY || 'testSecret', 
+  resave: false, 
+  saveUninitialized: false, 
+  cookie: {
+    maxAge: Number(process.env.SESSION_TIME) || 1800000 ,
+    httpOnly: true,
+    secure: false,
+
+  },
+  store: new RedisStore({ 
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: Number(process.env.REDIS_PORT || '6379'),
+    client: redisClient,
+    disableTouch: true
+   }) 
+}));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
   return res.send(
