74b9f5e85f861a5d239a4b8addf55e059ead2ed2
Kim HyunWoo <stories2@naver.com>
2022-04-27 01:18:51 +0900

    asdf

--- src/models/plugin.model.ts
+++ src/models/plugin.model.ts
@@ -1,28 +1,28 @@
 export interface PayloadPlugin {
-    pid?: number;
-    srid: number;
-    descript: string;
-    name: string;
-    url: string;
-    env: string;
+  pid?: number;
+  srid: number;
+  descript: string;
+  name: string;
+  url: string;
+  env: string;
 }
 
 export interface PluginInfoModel {
-    port: string;
-    url: string;
-    order: number;
-    env: string;
-    sampleOfResponse: any;
-    sampleOfRequest: any;
-    runtime_order_num?: number;
-    runtimeID?: number;
+  port: string;
+  url: string;
+  order: number;
+  env: string;
+  sampleOfResponse: any;
+  sampleOfRequest: any;
+  runtime_order_num?: number;
+  runtimeID?: number;
 }
 
 export interface PlugInImageModel {
-    imageID?: number;
-    name: string;
-    order_num?: number;
-    github_url: string;
-    registerDatetime: string;
-    updateDatetime: string;
+  imageID?: number;
+  name: string;
+  order_num?: number;
+  github_url: string;
+  registerDatetime?: string;
+  updateDatetime?: string;
 }
